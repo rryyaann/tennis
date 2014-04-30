40e0cc42ed7fa9abed5dc42d45c8833cf7035695
Ryan <rgwbarnes@gmail.com>
2014-04-30 12:06:01 -0700

    changed production environment for heroku experiment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Tennis::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Required for Devise. Remember to change localhost:3000 to actual application host
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'tenniscourtsponsorships.herokuapp.com' }
 end
