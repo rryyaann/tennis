210d47011394cc01f71e6755f083aaef9e4e9fb2
Ryan <rgwbarnes@gmail.com>
2014-05-24 16:31:41 -0700

    Finish setup for dropbox

--- app/models/court.rb
+++ app/models/court.rb
@@ -1,7 +1,13 @@
 class Court < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-	:storage => :dropbox,
-      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+        :storage => :dropbox,
+        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+        :path => ":style/:id_:filename"
+  end
+
 
 belongs_to :user
 end
