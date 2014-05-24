133a4026afe69467f64c96a8484edfcd5b3089bc
Ryan <rgwbarnes@gmail.com>
2014-05-23 18:48:10 -0700

    added google analytics code

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', '~> 3.0.3.0'
 gem 'devise'
 gem "paperclip", "~> 3.0"
+gem "paperclip-dropbox", ">= 1.1.7"
+
 
 group :production do
   gem 'pg'
--- app/models/court.rb
+++ app/models/court.rb
@@ -1,5 +1,7 @@
 class Court < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+	:storage => :dropbox,
+      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 
 belongs_to :user
 end
