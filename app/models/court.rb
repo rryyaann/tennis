0df13873e2371a46cf833e41465321e2dc5c268d
Ryan <rgwbarnes@gmail.com>
2014-05-24 16:35:41 -0700

    reverse image naming setup for now

--- app/models/court.rb
+++ app/models/court.rb
@@ -1,13 +1,7 @@
 class Court < ActiveRecord::Base
-	if Rails.env.development?
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-  else
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-        :storage => :dropbox,
-        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-        :path => ":style/:id_:filename"
-  end
-
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+	:storage => :dropbox,
+      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 
 belongs_to :user
 end
